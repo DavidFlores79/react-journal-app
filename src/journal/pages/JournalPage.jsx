940997866431ecb37aef73f0c24c3ac711c158d3
David Flores <david.flores@itsoft.mx>
2023-08-12 22:19:44 -0600

    Intendar codigo

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -10,7 +10,7 @@ export const JournalPage = () => {
         <>
             <JournalLayout>
                 {/* <NoteView/> */}
-                <DefaultView/>
+                <DefaultView />
 
 
 
@@ -19,14 +19,14 @@ export const JournalPage = () => {
                     sx={{
                         color: 'white',
                         backgroundColor: 'error.main',
-                        ':hover' : { backgroundColor: 'error.main', opacity: 0.8},
+                        ':hover': { backgroundColor: 'error.main', opacity: 0.8 },
                         position: 'fixed',
                         right: 50,
                         bottom: 50,
                     }}
                 >
 
-<AddOutlined sx={{ fontSize: 30 }}/>
+                    <AddOutlined sx={{ fontSize: 30 }} />
                 </IconButton>
             </JournalLayout>
         </>
